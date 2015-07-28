9fe1cdf2bc769dee67e0c16897049043bee02db5
Aubrey Wullschleger <aubrey.wullschleger@gmail.com>
2015-07-27 23:53:55 -0600

    updated app.use(session)(...)

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(session({
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: true }
+    cookie: { secure: false }
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -40,7 +40,6 @@ passport.use(new TwitterStrategy({
         callbackURL: process.env.HOST + "/auth/twitter/callback"
     },
     function(token, tokenSecret, profile, done) {
-        // asynchronous verification, for effect...
         process.nextTick(function () {
 
             // To keep the example simple, the user's Twitter profile is returned to
@@ -65,23 +64,11 @@ passport.use(new TwitterStrategy({
 //    res.render('login', { user: req.user });
 //});
 
-// GET /auth/twitter
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  The first step in Twitter authentication will involve redirecting
-//   the user to twitter.com.  After authorization, the Twitter will redirect
-//   the user back to this application at /auth/twitter/callback
 app.get('/auth/twitter',
     passport.authenticate('twitter'),
     function(req, res){
-        // The request will be redirected to Twitter for authentication, so this
-        // function will not be called.
     });
 
-// GET /auth/twitter/callback
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  If authentication fails, the user will be redirected back to the
-//   login page.  Otherwise, the primary route function function will be called,
-//   which, in this example, will redirect the user to the home page.
 app.get('/auth/twitter/callback',
     passport.authenticate('twitter', { failureRedirect: '/login' }),
     function(req, res) {
