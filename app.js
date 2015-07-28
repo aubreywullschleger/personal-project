2e2ffeec1986b42f1d8d6714cd02a7981699a6dc
Aubrey Wullschleger <aubrey.wullschleger@gmail.com>
2015-07-27 23:10:44 -0600

    oauth2 with twitter

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
+require('dotenv').load();
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var passport = require('passport');
+var TwitterStrategy = require('passport-twitter').Strategy;
 
 var app = express();
 
+app.set('trust proxy', 1);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -21,6 +25,87 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new TwitterStrategy({
+        consumerKey: process.env.TWITTER_CONSUMER_KEY,
+        consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+        callbackURL: "http://127.0.0.1:3000/auth/twitter/callback"
+    },
+    function(token, tokenSecret, profile, done) {
+        // asynchronous verification, for effect...
+        process.nextTick(function () {
+
+            // To keep the example simple, the user's Twitter profile is returned to
+            // represent the logged-in user.  In a typical application, you would want
+            // to associate the Twitter account with a user record in your database,
+            // and return that user instead.
+            return done(null, profile);
+        });
+    }
+));
+
+
+//app.get('/', function(req, res){
+//    res.render('index', { user: req.user });
+//});
+//
+//app.get('/account', ensureAuthenticated, function(req, res){
+//    res.render('account', { user: req.user });
+//});
+
+//app.get('/login', function(req, res){
+//    res.render('login', { user: req.user });
+//});
+
+// GET /auth/twitter
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in Twitter authentication will involve redirecting
+//   the user to twitter.com.  After authorization, the Twitter will redirect
+//   the user back to this application at /auth/twitter/callback
+app.get('/auth/twitter',
+    passport.authenticate('twitter'),
+    function(req, res){
+        // The request will be redirected to Twitter for authentication, so this
+        // function will not be called.
+    });
+
+// GET /auth/twitter/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+app.get('/auth/twitter/callback',
+    passport.authenticate('twitter', { failureRedirect: '/login' }),
+    function(req, res) {
+        res.redirect('/');
+    });
+
+//app.get('/logout', function(req, res){
+//    req.logout();
+//    res.redirect('/');
+//});
+
+//function ensureAuthenticated(req, res, next) {
+//    if (req.isAuthenticated()) { return next(); }
+//    res.redirect('/login')
+//}
+
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user)
+});
 
 app.use('/', routes);
 app.use('/users', users);
